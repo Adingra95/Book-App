dbe09a47d0cf6e50a712d7a21da333420028e9b6
M C <adingrafrancois95@gmail.com>
2022-04-22 09:49:06 +0530

    koffiApp

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.koffiApp',
+  appName: 'koffiApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
